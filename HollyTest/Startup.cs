e9580d1ca2759076f74dae97eaed2899ab1b129d
Hamish Murphy <hamishmurphy@gmail.com>
2019-07-09 14:00:56 +1000

    Based on Azure sign on

--- HollyTest/Startup.cs
+++ HollyTest/Startup.cs
@@ -2,21 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using HollyTest.Data;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.IdentityModel.Tokens;
+using HollyTest.Data;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace HollyTest
 {
@@ -33,51 +33,85 @@ namespace HollyTest
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>( options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")) );
-            services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddRazorPages();
-            services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
-
-            //services.AddCognitoIdentity();
-
-            services.Configure<OpenIdConnectOptions>(Configuration.GetSection("Authentication:Cognito"));
-
             var serviceProvider = services.BuildServiceProvider();
             var authOptions = serviceProvider.GetService<IOptions<OpenIdConnectOptions>>();
 
-            services.AddAuthentication(options =>
+            services.AddAuthentication( options =>
+                {
+                    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultSignOutScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                })
+                .AddCookie()
+                .AddOpenIdConnect(options => {
+                    options.ResponseType = OpenIdConnectResponseType.Code;
+                    options.MetadataAddress = "https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_QQIXmkpHD/.well-known/openid-configuration";
+                    options.ClientId = "238f6lc55s5titur3p1m2d7noa";
+                    options.ClientSecret = "3rvkl2hc2pl8ho4p1hdftr3a41meq8cslabgrm3289480kjnf8f";
+                    options.GetClaimsFromUserInfoEndpoint = true;
+                    //options.SaveTokens = authOptions.Value.SaveTokens;
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = false
+                    };
+                });
+            //services.Configure<OpenIdConnectOptions>(OpenIdConnectDefaults.AuthenticationScheme, options =>
+            //{
+            //    options.SignInScheme = "";
+            //    options.TokenValidationParameters = new TokenValidationParameters
+            //    {
+            //        // Instead of using the default validation (validating against a single issuer value, as we do in
+            //        // line of business apps), we inject our own multitenant validation logic
+            //        ValidateIssuer = false,
+
+            //        // If the app is meant to be accessed by entire organizations, add your issuer validation logic here.
+            //        //IssuerValidator = (issuer, securityToken, validationParameters) => {
+            //        //    if (myIssuerValidationLogic(issuer)) return issuer;
+            //        //}
+            //    };
+
+            //    options.Events = new OpenIdConnectEvents
+            //    {
+            //        OnTicketReceived = context =>
+            //        {
+            //            // If your authentication logic is based on users then add your logic here
+            //            return Task.CompletedTask;
+            //        },
+            //        OnAuthenticationFailed = context =>
+            //        {
+            //            context.Response.Redirect("/Error");
+            //            context.HandleResponse(); // Suppress the exception
+            //            return Task.CompletedTask;
+            //        },
+            //        // If your application needs to authenticate single users, add your user validation below.
+            //        //OnTokenValidated = context =>
+            //        //{
+            //        //    return myUserValidationLogic(context.Ticket.Principal);
+            //        //}
+            //    };
+            //});
+
+            services.AddControllersWithViews(options =>
             {
-                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
-            })
-            .AddCookie()
-            .AddOpenIdConnect(options =>
-            {
-                options.ResponseType = OpenIdConnectResponseType.Code;
-                options.MetadataAddress = authOptions.Value.MetadataAddress;
-                options.ClientId = authOptions.Value.ClientId;
-                options.ClientSecret = authOptions.Value.ClientSecret;
-                options.GetClaimsFromUserInfoEndpoint = true;
-                //options.SaveTokens = authOptions.Value.SaveTokens;
-                //options.TokenValidationParameters = new TokenValidationParameters
-                //{
-                //    ValidateIssuer = authOptions.Value.TokenValidationParameters.ValidateIssuer
-                //};
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                options.Filters.Add(new AuthorizeFilter(policy));
             });
+
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseAuthentication();
-            app.UseAuthorization();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
             }
             else
             {
@@ -90,6 +124,9 @@ namespace HollyTest
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
